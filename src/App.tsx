06485ec4dd3535a6b7f20dbea1b7fe55b0f0a82b
Jeffrey Nibbs <djcountry100@gmail.com>
2023-08-05 03:42:06 -0700

    chaged npm time packets

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
 import { useState } from 'react'
+import { DateTime } from 'luxon'
 
 function App() {
   const [currentTime, setCurrentTime] = useState('0')
-  const [dayTime, setDayTime] = useState('')
-  const date = new Date()
-
-  const padTo2Digits = (num: number) => {
-    return num.toString().padStart(2, '0')
-  }
 
   const setTime = () => {
-    setCurrentTime(
-      padTo2Digits(date.getHours() < 12 ? date.getHours() : date.getHours() - 12) +
-        ':' +
-        padTo2Digits(date.getMinutes()) +
-        ':' +
-        padTo2Digits(date.getSeconds())
-    )
-
-    if (date.getHours() < 12) {
-      setDayTime('AM')
-    } else {
-      setDayTime('PM')
-    }
+    const dt = DateTime.now()
+    setCurrentTime(dt)
   }
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button type="button" onClick={() => setTime()}>
+      <h1 className="text-3xl font-bold underline">Press If Baby Began Night Sleep Cycle</h1>
+      <button
+        className="border-2 border-blue-900 px-5 py-2 rounded-xl bg-gradient-to-br from-cyan-500 to-blue-900"
+        type="button"
+        onClick={() => setTime()}
+      >
         Get Time
       </button>
-      <p>
-        {currentTime.toLocaleString()} {dayTime}
-      </p>
+      <p>{currentTime === '0' ? 'Press the button to get the time' : 'Time is: ' + currentTime}</p>
+
+      {currentTime === '0' ? (
+        <p>Get Sleep Time First</p>
+      ) : (
+        <button className="border-2 border-red-900 px-5 py-2 rounded-xl bg-gradient-to-br from-purple-500 to-red-900" type="button">
+          Show Sleep Schedule
+        </button>
+      )}
     </>
   )
 }
